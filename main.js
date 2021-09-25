9e41856060051d138819cdd137bc2239be1d4930
Marina8989 <marina.mill209@gmail.com>
2021-09-25 16:05:40 -0700

    js drums

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 function playSound(e) {
      const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
+     const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+      
+     key.classList.toggle('playing');
+
+     if(!audio) return;
      audio.currentTime = 0;
      audio.play();
      
 }
+ 
 window.addEventListener('keydown', playSound)
